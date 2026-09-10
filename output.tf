99049228f7add4332733480aa3942313d970c16a
Trainerstacks <senthilvel121099@gmail.com>
2026-09-10 15:23:43 +0530

    Enhance EKS outputs with additional information
    Added outputs for cluster version, node group status, and security groups. Updated descriptions for existing outputs.

--- output.tf
+++ output.tf
@@ -1,44 +1,64 @@
 output "cluster_id" {
-  value = aws_eks_cluster.trainerstacks.id
+  value       = aws_eks_cluster.trainerstacks.id
   description = "EKS Cluster ID"
 }
 
 output "cluster_name" {
-  value = aws_eks_cluster.trainerstacks.name
+  value       = aws_eks_cluster.trainerstacks.name
   description = "EKS Cluster Name"
 }
 
 output "cluster_arn" {
-  value = aws_eks_cluster.trainerstacks.arn
+  value       = aws_eks_cluster.trainerstacks.arn
   description = "EKS Cluster ARN"
 }
 
 output "cluster_endpoint" {
-  value = aws_eks_cluster.trainerstacks.endpoint
+  value       = aws_eks_cluster.trainerstacks.endpoint
   description = "EKS Cluster Endpoint"
 }
 
+output "cluster_version" {
+  value       = aws_eks_cluster.trainerstacks.version
+  description = "EKS Cluster Version"
+}
+
 output "node_group_id" {
-  value = aws_eks_node_group.trainerstacks.id
+  value       = aws_eks_node_group.trainerstacks.id
   description = "EKS Node Group ID"
 }
 
+output "node_group_status" {
+  value       = aws_eks_node_group.trainerstacks.status
+  description = "Status of the EKS Node Group"
+}
+
 output "vpc_id" {
-  value = aws_vpc.trainerstacks_vpc.id
+  value       = aws_vpc.trainerstacks_vpc.id
   description = "VPC ID"
 }
 
 output "subnet_ids" {
-  value = aws_subnet.trainerstacks_subnet[*].id
-  description = "Subnet IDs"
+  value       = aws_subnet.trainerstacks_subnet[*].id
+  description = "List of Subnet IDs"
 }
 
-output "security_group_id" {
-  value = data.aws_security_group.eks_project_sg.id
-  description = "Security Group ID (eks_project_sg)"
+output "cluster_security_group_id" {
+  value       = aws_security_group.trainerstacks_cluster_sg.id
+  description = "Security Group ID for EKS Cluster"
+}
+
+output "node_security_group_id" {
+  value       = aws_security_group.trainerstacks_node_sg.id
+  description = "Security Group ID for EKS Nodes"
 }
 
 output "kubectl_config_command" {
-  value = "aws eks update-kubeconfig --name ${aws_eks_cluster.trainerstacks.name} --region ap-south-1"
-  description = "Command to configure kubectl"
+  value       = "aws eks update-kubeconfig --name ${aws_eks_cluster.trainerstacks.name} --region ap-south-1"
+  description = "Command to configure kubectl for the cluster"
+}
+
+output "cluster_oidc_issuer_url" {
+  value       = try(aws_eks_cluster.trainerstacks.identity[0].oidc[0].issuer, "")
+  description = "OIDC Issuer URL for the cluster (for IRSA setup later)"
 }
