11861a1ffc3b8f5bf01d4794ea9100f588b4cc85
Trainerstacks <senthilvel121099@gmail.com>
2026-09-10 15:16:22 +0530

    Add new outputs for EKS cluster and security group

--- output.tf
+++ output.tf
@@ -1,31 +1,44 @@
 output "cluster_id" {
   value = aws_eks_cluster.trainerstacks.id
+  description = "EKS Cluster ID"
 }
 
 output "cluster_name" {
   value = aws_eks_cluster.trainerstacks.name
+  description = "EKS Cluster Name"
 }
 
 output "cluster_arn" {
   value = aws_eks_cluster.trainerstacks.arn
+  description = "EKS Cluster ARN"
+}
+
+output "cluster_endpoint" {
+  value = aws_eks_cluster.trainerstacks.endpoint
+  description = "EKS Cluster Endpoint"
 }
 
 output "node_group_id" {
   value = aws_eks_node_group.trainerstacks.id
+  description = "EKS Node Group ID"
 }
 
 output "vpc_id" {
   value = aws_vpc.trainerstacks_vpc.id
+  description = "VPC ID"
 }
 
 output "subnet_ids" {
   value = aws_subnet.trainerstacks_subnet[*].id
+  description = "Subnet IDs"
 }
 
-output "cluster_security_group_id" {
-  value = aws_security_group.trainerstacks_cluster_sg.id
+output "security_group_id" {
+  value = data.aws_security_group.eks_project_sg.id
+  description = "Security Group ID (eks_project_sg)"
 }
 
-output "node_security_group_id" {
-  value = aws_security_group.trainerstacks_node_sg.id
+output "kubectl_config_command" {
+  value = "aws eks update-kubeconfig --name ${aws_eks_cluster.trainerstacks.name} --region ap-south-1"
+  description = "Command to configure kubectl"
 }
